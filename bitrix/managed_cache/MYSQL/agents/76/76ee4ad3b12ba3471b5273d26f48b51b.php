da28b1aa6b4284096900237f2e0b9188340494aa
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-21 01:24:20 +0500

    path fix google fonts to header.php

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001700510781';
-$dateexpire = '001700514441';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1700510840;}';
+$datecreate = '001700511684';
+$dateexpire = '001700515344';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1700511692;}';
 return true;
 ?>
