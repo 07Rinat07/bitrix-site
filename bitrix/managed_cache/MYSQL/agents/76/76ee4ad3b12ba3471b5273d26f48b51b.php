900e39ee87fd02808317bbc93925f50cf2789a35
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-21 01:08:08 +0500

    Correctly connect js to header.php

--- bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
+++ bitrix/managed_cache/MYSQL/agents/76/76ee4ad3b12ba3471b5273d26f48b51b.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001700510169';
-$dateexpire = '001700513829';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1700510228;}';
+$datecreate = '001700510781';
+$dateexpire = '001700514441';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1700510840;}';
 return true;
 ?>
--- local/templates/main/header.php
+++ local/templates/main/header.php
@@ -27,7 +27,22 @@ use Bitrix\Main\Page\Asset;
             Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/font-awesome.min.css');
             Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/style.css');
             Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/css/responsive.css');
-            Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . '/assets/js/vendor/modernizr-2.8.3.min.js');
+
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/vendor/modernizr-2.8.3.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/vendor/jquery-1.12.0.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/bootstrap.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/owl.carousel.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.counterup.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/waypoints.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.magnific-popup.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.mixitup.min.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.meanmenu.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.nav.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/jquery.parallax-1.1.3.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/animate-text.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/plugins.js');
+            Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/assets/js/main.js');
+
             ?>
             <?$APPLICATION->ShowHead();?>
         </head>
